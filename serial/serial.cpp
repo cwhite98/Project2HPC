1a24bca75ae6d10db6b4aadca28fc135ed62ce81
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-03-02 15:27:42 -0500

    Funciona 34-502

--- serial/serial.cpp
+++ serial/serial.cpp
@@ -4,63 +4,43 @@
 #include <sstream>
 #include <map>
 #include <string>
- 
+
 using namespace std;
 
-//Headers
-int wordCount(const char * input);
- 
-class WordCounter {
-public:
+class WordCounter
+{
+  public:
     int value;
-    WordCounter() : value( 0 ) {}
-     
-    void operator++ (int) { value++; }
+    WordCounter() : value(0) {}
+
+    void operator++(int) { value++; }
 };
- 
- 
-ostream& operator<<(ostream& st, WordCounter& wc) {
+
+typedef struct frecuency
+{
+    int frecuency;
+    string doc_id;
+    string title;
+} frecuency;
+
+ostream &operator<<(ostream &st, WordCounter &wc)
+{
     return st << wc.value;
 }
- 
-int main() {
-    const string path = "/Users/ronald/Documents/Project2HPC/serial/salida.csv";
-    ifstream ip(path);
-    if(!ip.is_open()) {
-        cout << "ERROR: File open" << '\n';
-        return 0;
-    }
 
-    string header;
-    string index;
-    string id;
-    string title;
-    string content;
-    getline(ip, header);
-    
-    //while(ip.good()) {
-        getline(ip, index, '\t');
-        getline(ip, id,     '\t');
-        getline(ip, title,  '\t');
-        getline(ip, content,'\n');
-        wordCount(content.c_str());
-    //}
-
-    ip.close();
-    return 0;
-}
-
-int wordCount(const char * input) {
+map<string, WordCounter> wordCount(const char *input)
+{
     map<string, WordCounter> counter;
-    
-    char * tok = strtok((char *) input, " ");
 
-    while (tok != NULL) {
+    char *tok = strtok((char *)input, " ");
+
+    while (tok != NULL)
+    {
         counter[tok]++;
-        printf ("%s\n", tok);
-        tok = strtok (NULL, " ");
+        //printf("%s\n", tok);
+        tok = strtok(NULL, " ");
     }
-    
+
     /**map< string, WordCounter,less<string> >::iterator it;
  
     for ( it  = counter.begin();
@@ -72,5 +52,66 @@ int wordCount(const char * input) {
              << (*it).second
              << endl;
     }*/
-    return 0; 
+    return counter;
+}
+
+int main()
+{
+    const string path = "/Users/ronald/Documents/Project2HPC/serial/salida.csv";
+    ifstream ip(path);
+    if (!ip.is_open())
+    {
+        cout << "ERROR: File open" << '\n';
+        return 0;
+    }
+
+    string header;
+    string index;
+    string id;
+    string title;
+    string content;
+    getline(ip, header);
+
+    map<string, map<string, frecuency> > table;
+    while (ip.good())
+    {
+        getline(ip, index, '\t');
+        getline(ip, id, '\t');
+        getline(ip, title, '\t');
+        getline(ip, content, '\n');
+
+        map<string, WordCounter> counter = wordCount(content.c_str());
+        map<string, WordCounter, less<string> >::iterator it;
+
+        for (it = counter.begin(); it != counter.end(); it++)
+        {
+            struct frecuency f;
+            f.frecuency = (*it).second.value;
+            f.doc_id = id;
+            f.title = title;
+
+            table[(*it).first][id] = f;
+        }
+
+        string search = "colombia";
+        map<string, frecuency> docs = table[search];
+        map<string, frecuency, less<string> >::iterator i;
+
+        for (i = docs.begin();
+             i != docs.end();
+             i++)
+        {
+            cout << (*i).first
+                 << ", "
+                 << (*i).second.frecuency
+                 << "   "
+                 << (*i).second.doc_id
+                 << "   "
+                 << (*i).second.title
+                 << endl;
+        }
+    }
+
+    ip.close();
+    return 0;
 }
