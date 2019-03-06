132bb10687f22fc02c4d2318c7ef271e0724e192
Camila White <camilawhite@MacBook-Pro-Camila.local>
2019-03-05 19:54:26 -0500

    serial is working

--- serial/serial.cpp
+++ serial/serial.cpp
@@ -18,6 +18,7 @@ class WordCounter
     void operator++(int) { value++; }
 };
 
+
 typedef struct frecuency
 {
     int frecuency;
@@ -29,6 +30,8 @@ ostream &operator<<(ostream &st, WordCounter &wc)
 {
     return st << wc.value;
 }
+
+map<string, map<string, frecuency> > table;
 
 map<string, WordCounter> wordCount(const char *input)
 {
@@ -57,9 +60,10 @@ map<string, WordCounter> wordCount(const char *input)
     return counter;
 }
 
-int main()
+int run(const string path)
 {
-    const string path = "/home/amonto69/Project2HPC/serial/1.csv";
+    //const string path = "/Users/camilawhite/Documents/Universidad/Semestre7/TopicosTelematica/apps/Project2HPC/serialersidad/
+    cout << path << endl;
     ifstream ip(path);
     if (!ip.is_open())
     {
@@ -74,7 +78,7 @@ int main()
     string content;
     getline(ip, header);
 
-    map<string, map<string, frecuency> > table;
+    //map<string, map<string, frecuency>> table;
     while (ip.good())
     {
         getline(ip, index, '\t');
@@ -85,7 +89,8 @@ int main()
         map<string, WordCounter> counter = wordCount(content.c_str());
         map<string, WordCounter, less<string> >::iterator it;
 
-        for (it = counter.begin(); it != counter.end(); it++) {
+        for (it = counter.begin(); it != counter.end(); it++)
+        {
             struct frecuency f;
             f.frecuency = (*it).second.value;
             f.doc_id = id;
@@ -94,34 +99,47 @@ int main()
             table[(*it).first][id] = f;
         }
     }
+    ip.close();
+    return 0;
+}
+
+int read() {
     string search;
-    cout << "Please enter a wordto search: ";
-    while(cin >> search) {
-        if(search == "/") break;
+    cout << "Please enter a word to search: ";
+    while (cin >> search)
+    {
+        if (search == "/")
+            break;
         transform(search.begin(), search.end(), search.begin(), ::tolower);
 
         map<string, frecuency> docs = table[search];
         map<string, frecuency, less<string> >::iterator i;
 
-        int suma=0;
+        int suma = 0;
 
-        for (i = docs.begin(); i != docs.end(); i++) {
+        for (i = docs.begin(); i != docs.end(); i++)
+        {
 
             suma += (*i).second.frecuency;
 
             cout << (*i).second.frecuency
-                << "   "
-                << (*i).second.doc_id
-                << "   "
-                << (*i).second.title
-                << endl;
-
+                 << "   "
+                 << (*i).second.doc_id
+                 << "   "
+                 << (*i).second.title
+                 << endl;
         }
 
         cout << "The word " << search << " is " << suma << " times in all news" << endl;
         cout << "Please enter a word to search: ";
     }
-
-    ip.close();
     return 0;
+}
+
+int main(int argc, char *argv[]) {
+    string files[3] = {"1.csv", "2.csv", "3.csv"};
+    for (int i = 0; i < 3; i++) {
+        run(files[i]);
+    }
+    read();
 }
