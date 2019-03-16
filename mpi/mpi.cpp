194a04dbe3718c2bcc4d239f017246fdd5f525cd
Ronald Cardona <ronaldkm98@gmail.com>
2019-03-16 18:57:47 -0500

    Rollback MPI

--- mpi/mpi.cpp
+++ mpi/mpi.cpp
@@ -22,33 +22,30 @@ public:
   void operator++(int) { value++; }
 };
 
-typedef struct frequency {
-  int frequency;
+typedef struct frecuency {
+  int frecuency;
   string doc_id;
   string title;
-} frequency;
+} frecuency;
 
 ostream &operator<<(ostream &st, WordCounter &wc) {
   return st << wc.value;
 }
 
 //Maps where all word freq are stored
-map<string, map<string,frequency> > table0;
-map<string, map<string, frequency> > table1;
-map<string, map<string, frequency> > table2;
+map<string, map<string, frecuency> > table0;
+map<string, map<string, frecuency> > table1;
+map<string, map<string, frecuency> > table2;
 
 //Maps where top 10 of the word are stored
-multimap<int, frequency, greater<int> > sorted0;
-multimap<int, frequency, greater<int> > sorted1;
-multimap<int, frequency, greater<int> > sorted2;
-
-//Total top 10
-multimap<int, frequency, greater<int> > table;
+multimap<int, frecuency, greater<int> > sorted0;
+multimap<int, frecuency, greater<int> > sorted1;
+multimap<int, frecuency, greater<int> > sorted2;
 
 MPI_Comm comm = MPI_COMM_WORLD;
-MPI_Status status;
 
 int range;
+
 int sumaTotal;
 
 map<string, WordCounter> wordCount(const char *input) {
@@ -63,6 +60,7 @@ map<string, WordCounter> wordCount(const char *input) {
   }
   return counter;
 }
+
 
 int run(const string path) {
   cout << path << endl;
@@ -92,11 +90,11 @@ int run(const string path) {
       }
 
       map<string, WordCounter> counter = wordCount(content.c_str());
-      map<string, WordCounter, less<string>>::iterator it;
+      map<string, WordCounter, less<string> >::iterator it;
 
       for (it = counter.begin(); it != counter.end(); it++) {
-        struct frequency f;
-        f.frequency = (*it).second.value;
+        struct frecuency f;
+        f.frecuency = (*it).second.value;
         f.doc_id = id;
         f.title = title;
 
@@ -105,12 +103,14 @@ int run(const string path) {
           {
             table0[(*it).first][id] = f;
           }
-        } else if (range == 1) {
+        }
+        else if (range == 1) {
           #pragma omp critical
           {
             table1[(*it).first][id] = f;
           }
-        } else if (range == 2) {
+        }
+        else if (range == 2) {
           #pragma omp critical
           {
             table2[(*it).first][id] = f;
@@ -123,86 +123,93 @@ int run(const string path) {
   return 0;
 }
 
-int sendTop10(multimap<int, frequency, greater<int> > sorted) {
-  int j = 0;
-  int freq = 0;
-  string sortId = "";
-  string sortTitle = "";
-  multimap<int, frequency>::iterator s;
-  for (s = sorted.begin(); s != sorted.end(); s++) {
-    if (j < 10) {
-      freq = (*s).first;
-      sortId = (*s).second.doc_id;
-      char sortedId[sortId.length() + 1];
-      strcpy(sortedId, sortId.c_str());
-
-      sortTitle = (*s).second.title;
-      char sortedTitle[sortTitle.length() + 1];
-      strcpy(sortedTitle, sortTitle.c_str());
-      MPI_Send(&freq, 1, MPI_INT, 0, 0, comm);
-      MPI_Send(&sortedId, 256, MPI_CHAR, 0, 0, comm);
-      MPI_Send(&sortedTitle, 256, MPI_CHAR, 0, 0, comm);
-    } else {
-      break;
-    }
-    j++;
-  }
-  return 0;
-}
 
 int read(string search) {
-  map<string, frequency> docs;
+  map<string, frecuency> docs;
   if (range == 0) {
     cout << "RANK 0" << endl;
     docs = table0[search];
-  } else if (range == 1) {
+  }
+  else if (range == 1) {
     cout << "RANK 1" << endl;
     docs = table1[search];
-  } else if (range == 2) {
+  }
+  else if (range == 2) {
     cout << "RANK 2" << endl;
     docs = table2[search];
   }
 
-  map<string, frequency, less<string>>::iterator i;
+
+  map<string, frecuency, less<string> >::iterator i;
   int suma = 0;
 
   for (i = docs.begin(); i != docs.end(); i++) {
     if (range == 0) {
-      sorted0.insert(pair<int, frequency>((*i).second.frequency, (*i).second));
+      sorted0.insert(pair<int, frecuency>((*i).second.frecuency, (*i).second));
     }
     else if (range == 1) {
-      sorted1.insert(pair<int, frequency>((*i).second.frequency, (*i).second));
+      sorted1.insert(pair<int, frecuency>((*i).second.frecuency, (*i).second));
     }
     else if (range == 2) {
-      sorted2.insert(pair<int, frequency>((*i).second.frequency, (*i).second));
+      sorted2.insert(pair<int, frecuency>((*i).second.frecuency, (*i).second));
     }
-    suma += (*i).second.frequency;
+    suma += (*i).second.frecuency;
   }
   sumaTotal += suma;
 
+  multimap<int, frecuency>::iterator s;
+  int j = 0;
+  int freq = 0;
+  string sortId = "";
+  string sortTitle = "";
   if (range == 1) {
-    sendTop10(sorted1);
-    MPI_Send(&suma, 1, MPI_INT, 0, 0, comm);
-  } else if (range == 2) {
-    sendTop10(sorted2);
+    for (s = sorted1.begin(); s != sorted1.end(); s++) {
+      if (j < 10) {
+        freq = (*s).first;
+        sortId = (*s).second.doc_id;
+        char sortedId[sortId.length() + 1];
+        strcpy(sortedId, sortId.c_str());
+
+        sortTitle = (*s).second.title;
+        char sortedTitle[sortTitle.length() + 1];
+        strcpy(sortedTitle, sortTitle.c_str());
+	//cout << "VOY A ENVIAR FREQ DE 1 ----- " << freq << endl;
+        MPI_Send(&freq, 1, MPI_INT, 0, 0, comm);
+        MPI_Send(&sortedId, 256, MPI_CHAR, 0, 0, comm);
+        MPI_Send(&sortedTitle, 256, MPI_CHAR, 0, 0, comm);
+	//cout << "TERMINE DE ENVIAR 1 " << endl;
+      }
+      else {
+        break;
+      }
+      j++;
+    }
     MPI_Send(&suma, 1, MPI_INT, 0, 0, comm);
   }
-  return 0;
-}
+  else if (range == 2) {
+    for (s = sorted2.begin(); s != sorted2.end(); s++) {
+      if (j < 10) {
+        freq = (*s).first;
+        sortId = (*s).second.doc_id;
+        char sortedId[sortId.length() + 1];
+        strcpy(sortedId, sortId.c_str());
 
-int getTop10(multimap<int, frequency, greater<int> > table, int source){
-  for (int i = 0; i < 10; i++) {
-    int freq;
-    char id[256];
-    char title[256];
-    MPI_Recv(&freq, 1, MPI_INT, source, 0, comm, &status);
-    MPI_Recv(&id, 256, MPI_CHAR, source, 0, comm, &status);
-    MPI_Recv(&title, 256, MPI_CHAR, source, 0, comm, &status);
-    struct frequency f;
-    f.frequency = freq;
-    f.doc_id = id;
-    f.title = title;
-    table.insert(pair<int, frequency>(freq, f));
+        sortTitle = (*s).second.title;
+        char sortedTitle[sortTitle.length() + 1];
+        strcpy(sortedTitle, sortTitle.c_str());
+	
+	//cout << "VOY A ENVIAR FREQ DE 2 ---  " << freq << endl;
+        MPI_Send(&freq, 1, MPI_INT, 0, 0, comm);
+        MPI_Send(&sortedId, 256, MPI_CHAR, 0, 0, comm);
+        MPI_Send(&sortedTitle, 256, MPI_CHAR, 0, 0, comm);
+	//cout << "TERMINE DE ENVIAR 2 " << endl;
+      }
+      else {
+        break;
+      }
+      j++;
+    }
+    MPI_Send(&suma, 1, MPI_INT, 0, 0, comm);
   }
   return 0;
 }
@@ -214,7 +221,7 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(comm, &range);
   MPI_Get_processor_name(name, &nameLen);
   MPI_Comm_size(comm, &size);
-  
+  MPI_Status status;
   cout << "Hello world from rank " << range << " running on " << name << endl;
 
   char *files[3];
@@ -232,9 +239,11 @@ int main(int argc, char *argv[]) {
   t0 = clock();
   if (range == 0) {
     run(files[0]);
-  } else if (range == 1) {
+  }
+  else if (range == 1) {
     run(files[1]);
-  } else if (range == 2) {
+  }
+  else if (range == 2) {
     run(files[2]);
   }
   t1 = clock();
@@ -242,6 +251,7 @@ int main(int argc, char *argv[]) {
   cout << "Execution Time building inverted index = " << time << endl;
 
   // Wait until all processes are done.
+
   int len = 256;
   bool work = true;
   while (work) {
@@ -252,69 +262,102 @@ int main(int argc, char *argv[]) {
       while (cin >> search) {
         if (search == "/") {
           work = false;
-          strcpy(word, search.c_str());
-          MPI_Send(&word, len, MPI_CHAR, 1, 0, comm);
-          MPI_Send(&word, len, MPI_CHAR, 2, 0, comm);
-          break;
-        }
-        transform(search.begin(), search.end(), search.begin(), ::tolower);
+	  strcpy(word, search.c_str());
+	  MPI_Send(&word, len, MPI_CHAR, 1, 0, comm);
+	  MPI_Send(&word, len, MPI_CHAR, 2, 0, comm);
+	  break;
+	}
+	transform(search.begin(), search.end(), search.begin(), ::tolower);
         strcpy(word, search.c_str());
         MPI_Send(&word, len, MPI_CHAR, 1, 0, comm);
         MPI_Send(&word, len, MPI_CHAR, 2, 0, comm);
         read(search);
-
-        // Store top 10 of all sorted top 10.
-        multimap<int, frequency>::iterator it;
-        int j = 0;
-        for (it = sorted0.begin(); it != sorted0.end(); it++) {
-          if (j < 10) {
-            table.insert(pair<int, frequency>((*it).first, (*it).second));
-          } else {
-            break;
-          }
-          j++;
+	
+	// Store top 10 of all sorted top 10.                                                           
+	multimap<int, frecuency, greater<int> > table;
+	multimap<int, frecuency>::iterator it;
+	int j = 0;
+	for (it = sorted0.begin(); it != sorted0.end(); it++) {
+	  if (j < 10) {
+	    table.insert(pair<int, frecuency>((*it).first, (*it).second));
+	  }
+	  else {
+	    break;
+	  }
+	  j++;
+	}
+	for (int i = 0; i < 10; i++) {
+	  int freq;
+	  char id[256];
+	  char title[256];
+	  MPI_Recv(&freq, 1, MPI_INT, 1, 0, comm, &status);
+	  //cout << "RECIBI FREQ 1: " << freq << endl;
+	  MPI_Recv(&id, 256, MPI_CHAR, 1, 0, comm, &status);
+	  MPI_Recv(&title, 256, MPI_CHAR, 1, 0, comm, &status);
+	  struct frecuency f;
+	  f.frecuency = freq;
+	  f.doc_id = id;
+	  f.title = title;
+	  table.insert(pair<int, frecuency>(freq, f));
+	}
+	int suma1;
+	MPI_Recv(&suma1, 1, MPI_INT, 1, 0, comm, &status);
+	sumaTotal += suma1;
+	for (int i = 0; i < 10; i++) {
+	  int freq;
+	  char id[256];
+	  char title[256];
+	  MPI_Recv(&freq, 1, MPI_INT, 2, 0, comm, &status);
+	  MPI_Recv(&id, 256, MPI_CHAR, 2, 0, comm, &status);
+	  MPI_Recv(&title, 256, MPI_CHAR, 2, 0, comm, &status);
+	  struct frecuency f;
+	  f.frecuency = freq;
+	  f.doc_id = id;
+	  f.title = title;
+	  table.insert(pair<int, frecuency>(freq, f));
         }
-        getTop10(table, 1);
-        int suma1;
-        MPI_Recv(&suma1, 1, MPI_INT, 1, 0, comm, &status);
-        sumaTotal += suma1;
-        getTop10(table, 2);
-        int suma2;
-        MPI_Recv(&suma2, 1, MPI_INT, 2, 0, comm, &status);
-        sumaTotal += suma2;
-        j = 0;
-        for (it = table.begin(); it != table.end(); it++) {
-          if (j < 10) {
-            cout << (*it).second.frequency << " "
-                 << (*it).second.doc_id << " "
-                 << (*it).second.title << endl;
-          } else break;
-          j++;
-        }
+	int suma2;
+	MPI_Recv(&suma2, 1, MPI_INT, 2, 0, comm, &status);
+	sumaTotal += suma2;
+	j = 0;
+	for(it = table.begin(); it != table.end(); it++) {
+          if(j < 10) {
+	  cout << (*it).second.frecuency << " "
+               << (*it).second.doc_id << " "
+               << (*it).second.title << endl;
+	  } else break;
+	  j++;
+	}
         cout << "The word " << search << " is " << sumaTotal << " times in all news" << endl;
-        sumaTotal = 0;
-        // Clean maps
-        sorted0.clear();
-        table.clear();
-        cout << "Please enter a word to search (press / to exit): ";
+	sumaTotal = 0;
+	// Clean maps
+	//	sorted0.erase(sorted0.begin(), sorted0.end());
+	//sorted1.erase(sorted1.begin(), sorted1.end());
+	//sorted2.erase(sorted2.begin(), sorted2.end());
+	//table.erase(table.begin(), table.end());
+	sorted0.clear();
+	table.clear();
+	cout << "Please enter a word to search (press / to exit): ";
       }
       work = false;
-    } else if (range == 1) {
+    }
+    else if (range == 1) {
       char inMsg[len];
       MPI_Recv(&inMsg, len, MPI_CHAR, 0, 0, comm, &status);
       //cout << "EL MENSAJE QUE LLEGO A 1 ES: " << inMsg << endl;
-      if (strcmp(inMsg, "/") == 0) {
-        break;
+      if(strcmp(inMsg, "/") == 0) {
+	break;
       }
       string search(inMsg);
       read(search);
       sorted1.clear();
-    } else if (range == 2) {
+    }
+    else if (range == 2) {
       char inMsg[len];
       MPI_Recv(&inMsg, len, MPI_CHAR, 0, 0, comm, &status);
       //cout << "EL MENSAJE QUE LLEGO A 2 ES: " << inMsg << endl;
-      if (strcmp(inMsg, "/") == 0) {
-        break;
+      if(strcmp(inMsg, "/") == 0) {
+	break;
       }
       string search(inMsg);
       read(search);
